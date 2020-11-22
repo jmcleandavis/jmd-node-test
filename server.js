ad97f66044b3de4620e51e2504bf165c13780d99
Jamie McLean-Davis <T875740@corp.ads>
2020-11-21 19:13:10 -0500

    rewrite

--- server.js
+++ server.js
@@ -1,4 +1,34 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(80, () => console.log('Example app listening on port 3000!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
+// app.listen(80, () => console.log('Example app listening on port 3000!'))
+
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+exports.helloWorld = (req, res) => {
+
+    const path = req.path;
+    switch(path){
+      case '/users':
+      console.log('Name:' . req.body.name);
+      res.status(200).send(validateUser(req,res));
+      break;
+      default: 
+        res.status(200).send('Please User /users End Point');
+    }
+    
+    };
+    
+    const validateUser = (req,res) => {
+      if (req.method === 'GET')
+      {
+        console.log('you got it' . req.body.name);
+        return 'You got it!' . req.body.name;
+      }else if (req.method !== 'GET') {
+        return 'use GET only';
+      }
+    }
